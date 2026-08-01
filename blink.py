7f0c8952dbe0632d97231768e88f20eaed31bf21
gtalbig <gtal5555@gmail.com>
2026-08-01 19:53:42 +0300

    Update blink.py

--- blink.py
+++ blink.py
@@ -4,13 +4,13 @@
 # On the Pico W the onboard LED is NOT on a fixed GPIO number.
 # It is wired to the wireless chip, so you address it by the name "LED".
 
-from machine import Pin
-from time import sleep
+   from machine import Pin
+   from time import sleep
 
-led = Pin("LED", Pin.OUT)   # "LED" = onboard LED on the Pico W
+   led = Pin("LED", Pin.OUT)   # onboard LED
 
-while True:
-    led.on()          # LED on
-    sleep(0.5)        # wait half a second
-    led.off()         # LED off
-    sleep(0.5)        # wait half a second
+   while True:
+       led.value(1)
+       time.sleep(1)
+       led.value(0)
+       time.sleep(1)
